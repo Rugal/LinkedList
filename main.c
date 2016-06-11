e1d0ca9817e03edfceec0415fbef8bc24334a6c1
hyhspaino <565271815@qq.com>
2016-06-11 07:24:45 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Administrator
+ *
+ * Created on 2016年6月7日, 下午9:07
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "node.h"
 #include "linked_list.h"
 
-
-int main()
+int
+main (int argc, char** argv)
 {
-    return 0;
+  linked_list* list = NULL;
+  list = create_linked_list ();
+  list = add_on_head (list);
+  list = input_data (list, 1, 23);
+  display_list (list);
+  printf ("!\n");
+  list = add_on_head (list);
+  list = input_data (list, 1, 4);
+  display_list (list);
+  printf ("!!\n");
+  list = add_on_tail (list);
+  list = input_data (list, 1, 7);
+  display_list (list);
+  printf ("!!!\n");
+  list = delete_one_node (list, 3);
+  display_list (list);
+  printf ("!!!!\n");
+  //  list = delete_one_node (list, 4);
+  //  display_list (list);
+  //  printf ("!!!!!\n");
+  list = delete_one_node (list, 2);
+  display_list (list);
+  printf ("!!!!!\n");
+  //  list = sort_list (list);
+  //  display_list (list);
+  //  printf ("!!!!\n");
+  list = add_on_head (list);
+  list = input_data (list, 1, 23);
+  display_list (list);
+  printf ("!\n");
+  list = add_on_head (list);
+  list = input_data (list, 1, 4);
+  display_list (list);
+  printf ("!!\n");
+  list = add_on_tail (list);
+  list = input_data (list, 1, 7);
+  printf ("!!!!!\n");
+  display_list (list);
+  list = sort_list (list);
+  display_list (list);
+  printf ("!!!!!\n");
+    delete_list (list);
+  printf ("!!!!\n");
+
+  return (EXIT_SUCCESS);
 }
