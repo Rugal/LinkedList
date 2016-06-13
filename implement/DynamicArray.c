a5ae3f3c91d9715270ec327f1d0f26aea403fcc8
hyhspaino <565271815@qq.com>
2016-06-13 00:48:25 -0400

    Create DynamicArray.c

---
+++ implement/DynamicArray.c
@@ -0,0 +1,107 @@
+#include "DynamicArray.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <malloc.h>
+
+dynamic_array*
+createArray (int size)
+{
+  dynamic_array * array = malloc (sizeof (dynamic_array));
+  array->data = malloc ((size * sizeof (int)));
+  memset (array->data, 0, (size * sizeof (int)));
+  array->size_of_array = size;
+  array->number_of_data = 0;
+  return array;
+}
+
+void
+cleanArray (dynamic_array* array)
+{
+  memset (array->data, 0, (array->size_of_array * sizeof (int)));
+
+}
+
+int
+indexAt (dynamic_array* array, int index)
+{
+  if (index < 1 || index > array->size_of_array)
+    {
+      printf ("It is not a correct position\n");
+      //    exit (1);
+      return false;
+    }
+  return array->data[index - 1];
+}
+
+void
+append (dynamic_array* array, int data)
+{
+  array->size_of_array++;
+  array->data = realloc (array->data, (array->size_of_array) * sizeof (int));
+  array->data[(array->size_of_array) - 1] = data;
+  array->number_of_data++;
+}
+
+void
+insertAt (dynamic_array* array, int data, int index)
+{
+  if (index < 1)
+    {
+      printf ("void insert position\n");
+      return;
+    }
+  array->number_of_data++;
+  while (index > array->size_of_array)
+    {
+      array->size_of_array = (array->size_of_array) * 2;
+      printf ("%d!!!!!!\n", array->size_of_array);
+      array->data = realloc (array->data, (array->size_of_array) * sizeof (int));
+    }
+  array->data[index - 1] = data;
+
+}
+
+void
+deleteAt (dynamic_array* array, int index)
+{
+  if (index < 1 || index > array->size_of_array)
+    {
+      printf ("void delete position\n");
+      return;
+    }
+
+  int temp = index - 1;
+  while (temp < array->size_of_array)
+    {
+      array->data[temp] = array->data[temp + 1];
+      temp++;
+    }
+  array->data = realloc (array->data, (array->size_of_array - 1) * sizeof (int));
+  array->size_of_array--;
+  array->number_of_data--;
+}
+
+void
+updateAt (dynamic_array* array, int data, int index)
+{
+  if (index > array->size_of_array || index < 1)
+    {
+      printf ("It is not a correct position\n");
+      return;
+    }
+  array->data[index - 1] = data;
+  printf ("!!!!\n");
+}
+
+void
+isEmpty (dynamic_array* array)
+{
+  if (0 == array->number_of_data)
+    {
+      printf ("this array is empty\n");
+      return;
+    }
+    printf ("this array is not empty\n");
+}
