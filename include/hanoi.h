f6cb689fd22744996313769c49f0a3f723902ea8
Rugal Bernstein <Rugal@users.noreply.github.com>
2016-06-15 22:34:08 -0400

    Create hanoi.h

---
+++ include/hanoi.h
@@ -0,0 +1,20 @@
+#ifndef DYNAMICARRAY_H
+#define DYNAMICARRAY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  /*
+    Hanio tower game.
+    Our mission is to move all plates from the 'Start' tower to 'Stop' tower by using 'temp' tower.
+    
+    @param plate The number of plates to be moved
+   */
+  void hanoi(int plate);
+  
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DYNAMICARRAY_H */
