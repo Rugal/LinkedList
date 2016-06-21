c2f047421ca9d66edc0301e6b638e459d3e39972
hyhspaino <565271815@qq.com>
2016-06-20 20:36:41 -0400

    Update hanoi.c

--- implement/hanoi.c
+++ implement/hanoi.c
@@ -7,26 +7,23 @@
 #include <stdio.h>
 // Our mission is to move all plates from the 'Start' tower(A) to 'Stop' tower(C) by using 'temp' tower(B).
 
-void
+static void
 Hanoi (int plate, char a, char b, char c)
 {
   if (1 == plate)
     {
-      printf ("move plate %d from %c to %c\n", plate, a, b);
-      printf ("move plate %d from %c to %c\n", plate, b, c);
+      printf ("move plate %d from %c to %c\n", plate, a, c);
     }
   else
     {
       Hanoi (plate - 1, a, b, c);
-      printf ("move plate %d from %c to %c\n", plate, a, b);
-      Hanoi (plate - 1, c, b, a);
-      printf ("move plate %d from %c to %c\n", plate, b, c);
-      Hanoi (plate - 1, a, b, c);
+      printf ("move plate %d from %c to %c\n", plate, a, c);
+      Hanoi (plate - 1, b, a, c);
     }
 }
 
 void
-hanoi (int plate)
+hanoi ( int plate)
 {
   Hanoi (plate, 'A', 'B', 'C');
 }
