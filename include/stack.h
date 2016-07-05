de47970874453825b59c424cdb74595f53d25f61
hyhspaino <565271815@qq.com>
2016-07-05 02:37:16 -0400

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -21,8 +21,9 @@ extern "C" {
 
     stack* create_stack(); // create a new stack and initialize this stack! WARNING:You crate a new demon. DEMON :Your soul will be tortured (ง •̀_•́)ง
     bool is_empty(stack * s);
-    void push(stack * s, int data); // add the new data on head of stack
-    int pop(stack* s); //get the first data and delete it from stack
+    void push(stack * s, int data); // add the new data on head of stack (DEMON subordinates 1)
+    int pop(stack* s); //get the first data and delete it from stack(DEMON subordinates 2)
+    int peek (stack * s)//get the first data (DEMON subordinates 3)
     void delete_stack(stack * s);
     /*  "You are making d!e!s!t!r!c!t!i!o!n"  punchline from "pony island" or just I create,but who cares? GOOD BYE STACK! <(￣︶￣)> 
      * DEMON: what about me?
@@ -35,3 +36,4 @@ extern "C" {
 #endif
 
 #endif /* STACK_H */
+
