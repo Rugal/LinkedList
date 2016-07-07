2d031772e7e15c07c7e725a74adba9ee2d1bad8f
hyhspaino <565271815@qq.com>
2016-07-07 03:51:02 -0400

    Update AVLTree.h

--- include/AVLTree.h
+++ include/AVLTree.h
@@ -1,3 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   AVLtree.h
+ * Author: Administrator
+ *
+ * Created on 2016年6月23日, 上午5:30
+ */
+
 #ifndef AVLTREE_H
 #define AVLTREE_H
 
@@ -5,36 +18,37 @@
 extern "C" {
 #endif
 
-  typedef struct AVLTree AVLTree;
-  
-  /*
-   * Instantiate an AVL tree object.
-   */
-  AVLTree* createTree();
 
-  /*
-   * Delete an AVL tree object.
-   */
-  void deleteTree(AVLTree*);
-  
-  /*
-   * Insert a data into AVL tree.
-   */
-  void put(AVLTree*, int data);
-  
-  /*
-   * Delete a node from AVL tree if it exists.
-   */
-  void remove(AVLTree*, int data);
-  
-  /*
-   * Traverse a tree, print it as top down, left to right order.
-   */
-  void traverse(AVLTree*);
-  
-  
+    typedef struct AVLtree AVLtree;
+
+    /*
+     * Instantiate an AVL tree object.
+     */
+    AVLtree* createTree();
+
+    /*
+     * Delete an AVL tree object.
+     */
+    void deleteTree(AVLtree* tree);
+
+    /*
+     * Insert a data into AVL tree.
+     */
+    void insert(AVLtree* tree, int data);
+
+    /*
+     * Delete a node from AVL tree if it exists.
+     */
+    void delete (AVLtree* tree, int data);
+    /*
+     * Traverse a tree, print it as top down, left to right order.
+     */
+    void traverse(AVLtree*);
+
+
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* AVLTREE_H */
+
